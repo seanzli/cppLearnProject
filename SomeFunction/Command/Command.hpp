88237c445d4bb205a5a440a2574241e4f3a9e23b
seanzli <seanzli@outlook.com>
2021-09-01 21:09:07 +0800

    use std::forward to forward arguments

--- SomeFunction/Command/Command.hpp
+++ SomeFunction/Command/Command.hpp
@@ -2,7 +2,7 @@
  * @Description: command template class
  * @Author: Sean
  * @Date: 2021-08-06 11:10:44
- * @LastEditTime: 2021-09-01 21:03:59
+ * @LastEditTime: 2021-09-01 21:07:29
  * @LastEditors: Please set LastEditors
  * @Reference: 
  */
@@ -20,15 +20,14 @@ public:
     ~BaseCommand() {}
 };
 
-///TODO consider use std::forward
 template<typename... ArgTypes>
 class Command : public BaseCommand {
     std::function<void(ArgTypes...)> func;
 public:
     explicit Command(std::function<void(ArgTypes...)> _func) : func(_func) {}
-    void operator()(ArgTypes... args) {
+    void operator()(ArgTypes&&... args) {
         if (func)
-            func(args...);
+            func(std::forward(args)...);
     }
 };
 
@@ -40,14 +39,14 @@ public:
         hash.insert(std::make_pair(name, std::shared_ptr<BaseCommand>(new T(func))));
     }
     template<typename... ArgTypes>
-    void execute(const std::string& name, ArgTypes... args) {
+    void execute(const std::string& name, ArgTypes&&... args) {
         std::unordered_map<std::string, std::shared_ptr<BaseCommand>>::const_iterator itr = hash.find(name);
 
         if (itr == hash.end())
             return;
         auto cur_cmd = (Command<ArgTypes...>*)(itr->second.get());
         if (cur_cmd)
-            (*cur_cmd)(args...);
+            (*cur_cmd)(std::forward(args)...);
     }
 
 private:
