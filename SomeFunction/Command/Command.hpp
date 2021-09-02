85d868ae8b1e15e6540e72184e9aacf6b736f3d8
seanzli <seanzli@outlook.com>
2021-09-02 20:32:00 +0800

    command test

--- SomeFunction/Command/Command.hpp
+++ SomeFunction/Command/Command.hpp
@@ -2,7 +2,7 @@
  * @Description: command template class
  * @Author: Sean
  * @Date: 2021-08-06 11:10:44
- * @LastEditTime: 2021-09-01 21:07:29
+ * @LastEditTime: 2021-09-02 20:27:11
  * @LastEditors: Please set LastEditors
  * @Reference: 
  */
@@ -25,9 +25,9 @@ class Command : public BaseCommand {
     std::function<void(ArgTypes...)> func;
 public:
     explicit Command(std::function<void(ArgTypes...)> _func) : func(_func) {}
-    void operator()(ArgTypes&&... args) {
+    void operator()(ArgTypes... args) {
         if (func)
-            func(std::forward(args)...);
+            func(args...);
     }
 };
 
@@ -39,14 +39,14 @@ public:
         hash.insert(std::make_pair(name, std::shared_ptr<BaseCommand>(new T(func))));
     }
     template<typename... ArgTypes>
-    void execute(const std::string& name, ArgTypes&&... args) {
+    void execute(const std::string& name, ArgTypes... args) {
         std::unordered_map<std::string, std::shared_ptr<BaseCommand>>::const_iterator itr = hash.find(name);
 
         if (itr == hash.end())
             return;
         auto cur_cmd = (Command<ArgTypes...>*)(itr->second.get());
         if (cur_cmd)
-            (*cur_cmd)(std::forward(args)...);
+            (*cur_cmd)(args...);
     }
 
 private:
---
+++ SomeFunction/Command/main.cpp
@@ -0,0 +1,83 @@
+/*
+ * @Author: Sean
+ * @Date: 2021-09-02 20:05:23
+ * @LastEditTime: 2021-09-02 20:30:39
+ * @LastEditors: Please set LastEditors
+ * @Description: Sean
+ * @FilePath: /cppLearnProject/SomeFunction/Command/main.cpp
+ */
+
+
+#include "Command.hpp"
+
+#include <iostream>
+
+void func1() {
+    printf("---\nno argument, void output\n");
+}
+
+void func2(int a) {
+    std::cout << "---\none argument input test.\n";
+    std::cout << "int argument a = " << a << "\n";
+}
+
+void func3(int a, int b) {
+    std::cout << "---\ntwo argument input test.\n";
+    std::cout << "int argument a = " << a << "\n";
+    std::cout << "int argument b = " << b << "\n";
+}
+
+int func4(int a, double b) {
+    std::cout << "---\ntwo argument input, int output test.\n";
+    std::cout << "int argument a = " << a << "\n";
+    std::cout << "int argument b = " << b << "\n";
+}
+
+struct FuncTest {
+    int a;
+    double b;
+};
+
+void func5(int a, FuncTest t) {
+    std::cout << "---\nStruct argument test\n";
+    std::cout << "int a = " << a << "\n";
+    std::cout << "struct t.a = " << t.a << "\n";
+    std::cout << "struct t.b = " << t.b << "\n";
+}
+
+void func6(int a, FuncTest& t) {
+    std::cout << "---\nStruct reference argument test\n";
+    std::cout << "int a = " << a << "\n";
+    std::cout << "struct t.a = " << t.a << "\n";
+    std::cout << "struct t.b = " << t.b << "\n";
+}
+
+int main() {
+    using namespace CommandSystem;
+
+    Manager m;
+
+    m.attach("func1", Command<>(func1));
+    m.attach("func2", Command<int>(func2));
+    m.attach("func3", Command<int, int>(func3));
+    m.attach("func4", Command<int, int>(func4));
+    m.attach("func5", Command<int, FuncTest>(func5));
+    m.attach("func6", Command<int, FuncTest&>(func6));
+
+    m.execute("func1");
+    m.execute("func2", 1);
+    m.execute("func3", 1, 2);
+    m.execute("func4", 1, 2);
+    FuncTest f{4,5};
+    m.execute("func5", 1, f);
+    m.execute("func6", 1, f);
+
+    // lambda
+    m.attach("func7", Command<>([](){printf("---\nLambda \n");}));
+    m.attach("func8", Command<int>([](int i){
+        printf("---\nLambda a = %d \n", i);}));
+    m.execute("func7");
+    m.execute("func8", 30);
+
+    return 0;
+}
