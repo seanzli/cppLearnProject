60f760a6d0d8a9adf3625c8ead2163e3de06523b
seanzli <seanzli@outlook.com>
2021-09-04 10:19:49 +0800

    add command with thread pool class, combin two class's interface

---
+++ SomeFunction/CommandWithThreadPool/CommandWithThreadPool.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file CommandWithThreadPool.hpp
+ * @author Sean 
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "../Command/Command.hpp"
+#include "../ThreadPool/threadPool.hpp"
+
+#include <memory>
+
+using pThreadPool = std::shared_ptr<ThreadPool>;
+
+class ManagerTP {
+public:
+    /**
+     * @brief Construct a new Manager T P object
+     * 
+     * @param size 
+     */
+    explicit ManagerTP(unsigned size) {
+        /// new ThreadPool pointer;
+        threadPool = pThreadPool(new ThreadPool);
+    }
+
+    /**
+     * @brief register name/function to managerThreadPool
+     * 
+     * @tparam T        // CommandSystem::Command<ArgType...>
+     * @param name      // register name
+     * @param f         // CommandSystem::Command<ArgType> function;
+     * @return true     // register success
+     * @return false    // register false
+     */
+    template<typename T>
+    bool attach(const std::string& name, const T& f) {
+        return cmdManager.attach(name, f);
+    }
+
+    /**
+     * @brief execute 
+     * 
+     * @tparam ArgType  :function input types
+     * @param name      :register function name
+     * @param args      :register function input arguments
+     * @return void
+     */
+    template<typename... ArgType>
+    void execute(const std::string& name, ArgType&&... args) {
+        auto cmd = cmdManager.getCommand(name);
+        if (cmd == nullptr)
+            return;
+        auto func = ((CommandSystem::Command<ArgType...>*)(cmd))->getFunction();
+
+        threadPool->attach(func, std::forward<ArgType>(args)...);
+        return;
+    }
+
+private:
+    pThreadPool threadPool;
+    CommandSystem::Manager cmdManager; /// just use map function
+};
---
+++ SomeFunction/CommandWithThreadPool/main.cpp
@@ -0,0 +1,76 @@
+#include "CommandWithThreadPool.hpp"
+
+#include <iostream>
+
+using namespace CommandSystem;
+
+void func1() {
+    printf("---\nno argument, void output\n");
+}
+
+void func2(int a) {
+    std::cout << "---\none argument input test.\n";
+    std::cout << "int argument a = " << a << "\n";
+}
+
+void func3(int a, int b) {
+    std::cout << "---\ntwo argument input test.\n";
+    std::cout << "int argument a = " << a << "\n";
+    std::cout << "int argument b = " << b << "\n";
+}
+
+int func4(int a, double b) {
+    std::cout << "---\ntwo argument input, int output test.\n";
+    std::cout << "int argument a = " << a << "\n";
+    std::cout << "int argument b = " << b << "\n";
+}
+
+struct FuncTest {
+    int a;
+    double b;
+};
+
+void func5(int a, FuncTest t) {
+    std::cout << "---\nStruct argument test\n";
+    std::cout << "int a = " << a << "\n";
+    std::cout << "struct t.a = " << t.a << "\n";
+    std::cout << "struct t.b = " << t.b << "\n";
+}
+
+void func6(int a, FuncTest& t) {
+    std::cout << "---\nStruct reference argument test\n";
+    std::cout << "int a = " << a << "\n";
+    std::cout << "struct t.a = " << t.a << "\n";
+    std::cout << "struct t.b = " << t.b << "\n";
+}
+
+int main() {
+    ManagerTP m(4);
+
+    m.attach("func1", Command<>(func1));
+    m.attach("func2", Command<int>(func2));
+    m.attach("func3", Command<int, int>(func3));
+    m.attach("func4", Command<int, int>(func4));
+    m.attach("func5", Command<int, FuncTest>(func5));
+    m.attach("func6", Command<int, FuncTest&>(func6));
+
+
+
+    // lambda
+    m.attach("func7", Command<>([](){printf("---\nLambda \n");}));
+    m.attach("func8", Command<int>([](int i){
+        printf("---\nLambda a = %d \n", i);}));
+
+
+    m.execute("func1");
+    m.execute("func2", 1);
+    m.execute("func3", 1, 2);
+    m.execute("func4", 1, 2);
+    FuncTest f{4,5};
+    m.execute("func5", 1, f);
+    m.execute("func6", 1, f);
+    m.execute("func7");
+    m.execute("func8", 30);
+
+    system("Pause");
+}
