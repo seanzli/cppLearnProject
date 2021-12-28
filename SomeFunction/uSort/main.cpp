db88a17f5eb8e8f24f11a1e307c2781893abcc7a
mu <muxzuo@outlook.com>
2021-12-28 20:56:50 +0800

    merge sort, improve efficiency

--- SomeFunction/uSort/main.cpp
+++ SomeFunction/uSort/main.cpp
@@ -5,8 +5,9 @@
 int main() {
     std::vector<int> a = {5,7,19,4,6};
     auto lam = [](int a, int b) {return a < b;};
-    sort(a, lam);
+//    sort(a, lam);
 
+    sort(a);
     for (auto& itr : a) {
         std::cout << itr << ", ";
     }
--- SomeFunction/uSort/usort.hpp
+++ SomeFunction/uSort/usort.hpp
@@ -1,41 +1,75 @@
 #include <vector>
 #include <functional>
+//
+//template<typename T>
+//void cut(const std::vector<T>& in, std::vector<T>& left, std::vector<T>& right) {
+//    size_t size = in.size();
+//    left = std::vector<T>(in.begin(), in.begin() + size / 2);
+//    right = std::vector<T>(in.begin() + size / 2, in.end());
+//}
+//
+//template<typename T, typename Cmp>
+//void merge(const std::vector<T>& left,
+//           const std::vector<T>& right,
+//           std::vector<T>& out,
+//           Cmp& fun) {
+//    size_t idx_left = 0, idx_right = 0;
+//    out.clear();
+//    while (idx_left < left.size() && idx_right < right.size()) {
+//        out.push_back(fun(left[idx_left], right[idx_right])
+//                ? left[idx_left++]
+//                : right[idx_right++]);
+//    }
+//    while (idx_left < left.size())
+//        out.push_back(left[idx_left++]);
+//    while (idx_right < right.size())
+//        out.push_back(right[idx_right++]);
+//}
+//
+//template<typename T, typename Comp>
+//void sort(std::vector<T>& in, Comp& fun) {
+//    if (in.size() < 2)
+//        return;
+//    std::vector<T> left, right;
+//    cut(in, left, right);
+//    sort(left, fun);
+//    sort(right, fun);
+//    merge(left, right, in, fun);
+//    return;
+//}
 
-
-
-template<typename T>
-void cut(const std::vector<T>& in, std::vector<T>& left, std::vector<T>& right) {
-    size_t size = in.size();
-    left = std::vector<T>(in.begin(), in.begin() + size / 2);
-    right = std::vector<T>(in.begin() + size / 2, in.end());
+template<typename T, typename Cmp>
+void merge(std::vector<T>& in, size_t left, size_t mid, size_t right, Cmp& fun) {
+    std::vector<T> cur;
+    cur.reserve(right - left + 1);
+    size_t idx1 = left, idx2 = mid + 1;
+    while (idx1 <= mid && idx2 <= right) {
+        cur.push_back(
+                fun(in[idx1], in[idx2]) ? in[idx1++] : in[idx2++]
+                );
+    }
+    while (idx1 <= mid)
+        cur.push_back(in[idx1++]);
+    while (idx2 <= right)
+        cur.push_back(in[idx2++]);
+    for (size_t i = 0; i < cur.size(); ++i) {
+        in[i + left] = cur[i];
+    }
 }
 
 template<typename T, typename Cmp>
-void merge(const std::vector<T>& left,
-           const std::vector<T>& right,
-           std::vector<T>& out,
-           Cmp& fun) {
-    size_t idx_left = 0, idx_right = 0;
-    out.clear();
-    while (idx_left < left.size() && idx_right < right.size()) {
-        out.push_back(fun(left[idx_left], right[idx_right])
-                ? left[idx_left++]
-                : right[idx_right++]);
-    }
-    while (idx_left < left.size())
-        out.push_back(left[idx_left++]);
-    while (idx_right < right.size())
-        out.push_back(right[idx_right++]);
+void sort(std::vector<T>& in, size_t left, size_t right, Cmp& fun) {
+    if (left >= right)
+        return;
+    size_t mid = (left + right) >> 1;
+    sort(in, left, mid, fun);
+    sort(in, mid+1, right, fun);
+    merge(in, left, mid, right, fun);
 }
 
-template<typename T, typename Comp>
-void sort(std::vector<T>& in, Comp& fun) {
+template<typename T, typename Cmp = std::function<bool(T, T)>>
+void sort(std::vector<T>& in, Cmp fun = ::std::less<T>()) {
     if (in.size() < 2)
         return;
-    std::vector<T> left, right;
-    cut(in, left, right);
-    sort(left, fun);
-    sort(right, fun);
-    merge(left, right, in, fun);
-    return;
+    sort(in, 0, in.size() - 1, fun);
 }
