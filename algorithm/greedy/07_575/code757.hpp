bd476fde61e95f59e00073bcc62507f76231a40c
nick <muxzuo@outlook.com>
2023-02-07 19:35:06 +0800

    day 07

---
+++ algorithm/greedy/07_575/code757.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int distributeCandies(vector<int> &candyType) {
+        return min(unordered_set<int>(candyType.begin(), candyType.end()).size(), candyType.size() / 2);
+    }
+};
