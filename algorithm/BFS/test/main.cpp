59a4aa12e53fcf5a64399ed739fa13332791a0cc
nick <muxzuo@outlook.com>
2023-03-12 11:40:20 +0800

    dfs day05 301

--- algorithm/BFS/test/main.cpp
+++ algorithm/BFS/test/main.cpp
@@ -4,8 +4,9 @@
 int main()
 {
     Solution solution;
-    vector<vector<int>> nums = {{1,6,1},{5,8,7},{1,9,1}};
-    auto out = solution.getMaximumGold(nums);
-    std::cout << out << "\n";
+    // vector<vector<int>> nums = {{1,6,1},{5,8,7},{1,9,1}};
+    std::string str = "(a))";
+    auto out = solution.removeInvalidParentheses(str);
+    // std::cout << out << "\n";
     return 0;
 }
