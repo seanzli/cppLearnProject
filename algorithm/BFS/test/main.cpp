79031f0c535012c3a1160939dfefb0180f56c8af
nick <muxzuo@outlook.com>
2023-03-13 20:27:58 +0800

    dfs day06 437

--- algorithm/BFS/test/main.cpp
+++ algorithm/BFS/test/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
     Solution solution;
     // vector<vector<int>> nums = {{1,6,1},{5,8,7},{1,9,1}};
-    std::string str = "(a))";
-    auto out = solution.removeInvalidParentheses(str);
+    TreeNode* str = new TreeNode(10, new TreeNode(5, new TreeNode(3), new TreeNode(2, nullptr, new TreeNode(1))), nullptr);
+    auto out = solution.pathSum(str, 8);
     // std::cout << out << "\n";
     return 0;
 }
