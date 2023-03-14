106811e71c8dbb085be9183cc708873e98cb8453
nick <muxzuo@outlook.com>
2023-03-14 21:19:09 +0800

    dfs day 07 1239

--- algorithm/BFS/test/main.cpp
+++ algorithm/BFS/test/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
     Solution solution;
     // vector<vector<int>> nums = {{1,6,1},{5,8,7},{1,9,1}};
-    TreeNode* str = new TreeNode(3, new TreeNode(9, nullptr, nullptr), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-    auto out = solution.verticalTraversal(str);
+    vector<string> str = {"un", "iq", "ue"};
+    auto out = solution.maxLength(str);
     // std::cout << out << "\n";
     return 0;
 }
